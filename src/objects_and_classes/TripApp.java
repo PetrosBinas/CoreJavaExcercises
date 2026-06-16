0cff306d7769454dc9e76d2ac32a8bb948fc5531
PetrosBinas <ptrsbns@gmail.com>
2026-06-16 22:02:33 +0300

    TripApp init

---
+++ src/objects_and_classes/TripApp.java
@@ -0,0 +1,18 @@
+package objects_and_classes;
+
+public class TripApp {
+
+    static void main() {
+
+    }
+
+}
+
+class Car {
+
+    private String brand;
+    private String model;
+    private double consumption;
+    private double fuelCapacity;
+
+}
