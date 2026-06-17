22274a5ac85b6c4ca6b051a2378e7697863db8d3
PetrosBinas <ptrsbns@gmail.com>
2026-06-17 20:02:25 +0300

    Basic Car Methods

--- src/objects_and_classes/TripApp.java
+++ src/objects_and_classes/TripApp.java
@@ -15,4 +15,31 @@ class Car {
     private double consumption;
     private double fuelCapacity;
 
+    //Constructor
+    public Car(String b, String m, double c, double f) {
+
+        this.brand = b;
+        this.model = m;
+        this.consumption = c;
+        this.fuelCapacity = f;
+    }
+
+    //getters
+    public String getBrand() { return this.brand; }
+    public String getModel() { return this.model; }
+    public double getConsumption() { return this.consumption; }
+    public double getFuelCapacity() { return this.fuelCapacity; }
+
+    //setters
+    public void setBrand(String b) { this.brand = b; }
+    public void setModel(String m) { this.model = m; }
+    public void setConsumption(double c) { this.consumption = c; }
+    public void setFuelCapacity(double f) { this.fuelCapacity = f; }
+
+    //methods
+    public double maxKm() {
+
+        double kmPerLiter = 100 / consumption;
+        double maxIndependencyOfCar = kmPerLiter * fuelCapacity;
+    }
 }
