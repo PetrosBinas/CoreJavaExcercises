6f6db602419f4bbc10e3e06eaf79d84acaed1075
PetrosBinas <ptrsbns@gmail.com>
2026-06-18 21:40:23 +0300

    main basic logic

--- src/objects_and_classes/TripApp.java
+++ src/objects_and_classes/TripApp.java
@@ -1,9 +1,80 @@
 package objects_and_classes;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TripApp {
 
     static void main() {
 
+        Scanner sc = new Scanner(System.in);
+        Car clientCar = clientPrompt(sc);
+    }
+
+    static Car clientPrompt(Scanner sc) {
+
+        String brand;
+        String model;
+        double consumption;
+        double fuelCapacity;
+
+        while(true) {
+             try {
+                 System.out.println("Type Your Car's Brand:");
+                 brand = sc.nextLine();
+                 break;
+             }
+             catch (Exception e) {
+                 System.out.println("Brand Field Cannot be Empty!");
+             }
+        }
+
+        while(true) {
+            try {
+                System.out.println("Type your Car's Model:");
+                model = sc.nextLine();
+                if(!model.isBlank()) { break; }
+            }
+            catch (Exception e) {
+                System.out.println("Model Field Cannot be Empty!");
+            }
+        }
+
+        while(true) {
+            try {
+                System.out.println("Type The Consumption of Your Car per 100 km:");
+                consumption = sc.nextDouble();
+                if(consumption <= 0) {
+                    throw new Exception();
+                }
+                break;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Type A Valid Number for Your Vehicle Consumption!");
+            }
+            catch (Exception e) {
+                System.out.println("Consumption Cannot Be a Negative Number!");
+            }
+        }
+
+        while(true) {
+            try {
+                System.out.println("Type The Fuel Capacity of Your Car:");
+                fuelCapacity = sc.nextDouble();
+                if(fuelCapacity <= 0) {
+                    throw new Exception();
+                }
+                break;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Type a Valid Number for Your Vehicle Fuel Capacity!");
+            }
+            catch (Exception e) {
+                System.out.println("Fuel Capacity Cannot be a Negative Number!");
+            }
+        }
+
+        return new Car(brand, model, consumption, fuelCapacity);
     }
 
 }
@@ -40,6 +111,6 @@ class Car {
     public double maxKm() {
 
         double kmPerLiter = 100 / consumption;
-        double maxIndependencyOfCar = kmPerLiter * fuelCapacity;
+        return kmPerLiter * fuelCapacity; //returning max Km the Car Object can travel with its autonomy
     }
 }
