673bd626e8d6bcb903d32b731f1587f869038f84
PetrosBinas <ptrsbns@gmail.com>
2026-06-18 23:40:00 +0300

    Duel Game Init

---
+++ src/objects_and_classes/DuelGame.java
@@ -0,0 +1,31 @@
+package objects_and_classes;
+
+public class DuelGame {
+
+    static void main() {
+
+    }
+
+}
+
+class Character {
+
+    private String name;
+    private int health;
+    private int attackPower;
+    private int defence;
+
+    //constructor
+    public Character(String n, int h, int a, int d) {
+
+        this.name = n;
+        this.health = h;
+        this.attackPower = a;
+        this.defence = d;
+    }
+
+    //getters
+
+    //setters
+
+}
