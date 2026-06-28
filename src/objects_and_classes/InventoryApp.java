26e77160fc0bf08405fc75e276593ce820b3bf9e
PetrosBinas <ptrsbns@gmail.com>
2026-06-28 22:00:51 +0300

    InventoryApp Init

---
+++ src/objects_and_classes/InventoryApp.java
@@ -0,0 +1,23 @@
+package objects_and_classes;
+
+public class InventoryApp {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Product {
+
+    String name;
+    double price;
+    long quantity;
+
+}
+
+class Inventory {
+
+    Product[] products;
+    long productCount;
+
+}
